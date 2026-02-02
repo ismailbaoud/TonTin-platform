d91d8a39584f1e03c23084203212a4afea6fe677
ismail baoud <ibaoud46@egmail.com>
2026-02-02 16:41:37 +0100

    feat: implement createMember method in MemberService and MemberServiceImpl

--- platform-back/src/main/java/com/tontin/platform/service/MemberService.java
+++ platform-back/src/main/java/com/tontin/platform/service/MemberService.java
@@ -3,10 +3,14 @@ package com.tontin.platform.service;
 import java.util.List;
 import java.util.UUID;
 
+import com.tontin.platform.domain.Dart;
+import com.tontin.platform.domain.enums.dart.DartPermession;
+import com.tontin.platform.domain.enums.member.MemberStatus;
 import com.tontin.platform.dto.member.request.MemberRequest;
 import com.tontin.platform.dto.member.response.MemberResponse;
 
 public interface MemberService {
+    void createMember(DartPermession permession , MemberStatus status, Dart dart);
     MemberResponse addMemberToDart(MemberRequest request, UUID id, UUID dartId);
     MemberResponse updateMemberPermession(MemberRequest request, UUID id , UUID dartId);
     MemberResponse getMember(UUID id, UUID dartId);
--- platform-back/src/main/java/com/tontin/platform/service/impl/MemberServiceImpl.java
+++ platform-back/src/main/java/com/tontin/platform/service/impl/MemberServiceImpl.java
@@ -1,14 +1,24 @@
 package com.tontin.platform.service.impl;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import com.tontin.platform.domain.Dart;
+import com.tontin.platform.domain.Member;
+import com.tontin.platform.domain.User;
+import com.tontin.platform.domain.enums.dart.DartPermession;
+import com.tontin.platform.domain.enums.dart.DartStatus;
+import com.tontin.platform.domain.enums.member.MemberStatus;
 import com.tontin.platform.dto.member.request.MemberRequest;
 import com.tontin.platform.dto.member.response.MemberResponse;
 import com.tontin.platform.mapper.user.DartMapper;
+import com.tontin.platform.mapper.user.MemberMapper;
 import com.tontin.platform.repository.MemberRepository;
+import com.tontin.platform.repository.UserRepository;
 import com.tontin.platform.service.MemberService;
 
 import lombok.RequiredArgsConstructor;
@@ -18,8 +28,22 @@ import lombok.RequiredArgsConstructor;
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
-    private final DartMapper dartMapper;
-    
+    private final MemberMapper memberMapper;
+    private final UserRepository userRepository;
+
+    @Override
+    public void createMember(DartPermession permession , MemberStatus status, Dart dart) {
+        User user = userRepository.findById(UUID.fromString("2995b69a-f056-4b56-8884-2647a4eadeb9")).orElseThrow();
+        Member member = Member.builder()
+        .joinedAt(LocalDateTime.now())
+        .permession(permession)
+        .status(status)
+        .dart(dart)
+        .user(user)
+        .build();
+        memberRepository.save(member);
+    }
+
     @Override
     public MemberResponse addMemberToDart(MemberRequest request, UUID id, UUID dartId) {
         // TODO Auto-generated method stub
